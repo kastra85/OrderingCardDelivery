e7d378c4cecbb654d00c6c8cbad0687aa618297e
Neto Nikolay <nikolay.kastranets.1985@gmail.com>
2023-10-04 21:56:11 +0300

    OrderingCardDelivery4

--- src/test/java/ru/netology/test/DataGeneratorTest.java
+++ src/test/java/ru/netology/test/DataGeneratorTest.java
@@ -37,18 +37,18 @@ public class DataGeneratorTest {
 
         $(byText("Запланировать")).click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='success-notification'] .notification_content")
-                .shouldHave(exactText("Встреча успешно запланирована на" + firstMeetingDate))
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + firstMeetingDate))
                 .shouldBe(visible);
         $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
         $("[data-test-id=date] input").setValue(secondMeetingDate);
         $(byText("Запланировать")).click();
-        $("[data-test-id='replan-notification'] .notification_content")
+        $("[data-test-id='replan-notification'] .notification__content")
                 .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"))
                 .shouldBe(visible);
         $("[data-test-id='replan-notification'] button").click();
-        $("[data-test-id='success-notification'] .notification_content")
-                .shouldHave(exactText("Встреча успешно запланирована на" + secondMeetingDate))
+        $("[data-test-id='success-notification'] .notification__content")
+                .shouldHave(exactText("Встреча успешно запланирована на " + secondMeetingDate))
                 .shouldBe(visible);
 
     }
